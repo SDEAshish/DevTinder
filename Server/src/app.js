6fbf2631ad704fc4db0b0adafff35144d998b6e3
ASHISH VISHWAKARMA <75712767+Aksharma2799@users.noreply.github.com>
2026-02-24 03:12:40 +0530

    initial

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,1 +1,13 @@
-console.log("Hi")
+const express = require('express');
+
+
+const app = express();
+
+app.use("/home", (req, res) =>{
+    res.send("Welcome to home page")
+    });
+
+
+app.listen(3001, ()=> {
+    console.log("App started")
+});
